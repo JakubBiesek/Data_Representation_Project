e2e97f1b0d8079fc27adc61f264180b32f64b378
JakubBiesek <g00313966@gmit.ie>
2016-12-01 00:31:57 +0100

    Update project.py

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'test'
+app.config['MYSQL_DB'] = 'flaskdb'
 mysql = MySQL(app)
 #: database queries which are called on index file by jQuery feature
 @app.route('/db')
